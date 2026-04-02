88213d44e32b74f4d56daa0f9c644b63325e3fc7
Joshua Jones <Joshua@jfns.info>
2026-04-02 17:39:23 -0600

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../vendor/autoload.php';   // ✅ Add this line first
+require_once '../core/functions.php';
+require_once '../core/app.php';
+require_once '../core/controller.php';
+require_once '../core/config.php';
+
+$app = new App();
